6c1c564b04ea68fb20af6b8bf31ce22fffc90991
oriol_llorens <oriol.6.vng@gmail.com>
2022-11-11 15:51:31 +0000

    Update almacenamiento.js

--- Producto 2 Solweb/almacenamiento.js
+++ Producto 2 Solweb/almacenamiento.js
@@ -1,52 +1,44 @@
 function inicio() {
+  const email = document.getElementById("Email").value;
+  const contraseña = document.getElementById("Contraseña").value;
+  const avatar = document.getElementById("Avatar").value;
+  const gameRoom = document.getElementById("GameRoom").value;
 
-    const email = document.getElementById('Email').value;
-    const contraseña = document.getElementById('Contraseña').value;
-    const avatar = document.getElementById('Avatar').value;
-    const gameRoom = document.getElementById('GameRoom').value;
+  console.log(email, contraseña, avatar, gameRoom);
 
-
-    console.log(email, contraseña, avatar, gameRoom);
-
-    if ((email === 'lais@lais.com' && contraseña === 'hola') || (email === 'hugo@hugo.com' && contraseña === 'hugo') ) {
-        window.location.href = 'lobby.html';
-        localStorage.email = email; 
-        localStorage.contraseña = contraseña;
-        localStorage.avatar = avatar;
-        localStorage.gameRoom = gameRoom;
-
-
-    }else{
-        alert('Email o contraseña incorrectos')
-    }
+  if (
+    (email === "lais@lais.com" && contraseña === "hola") ||
+    (email === "oriol@oriol.com" && contraseña === "oriol")
+  ) {
+    window.location.href = "lobby.html";
+    localStorage.email = email;
+    localStorage.contraseña = contraseña;
+    localStorage.avatar = avatar;
+    localStorage.gameRoom = gameRoom;
+  } else {
+    alert("Email o contraseña incorrectos");
+  }
 }
 
-document.querySelector('#botonRegistro').addEventListener('click', registro);
-
+document.querySelector("#botonRegistro").addEventListener("click", registro);
 
 function registro() {
-    var sUsuario = '',
-        sAvatar = '',
-        sEmail = '',
-        sContraseña = '',
-        sGameRoom = '',
-    
-    sUsuario = document.querySelector('#Usuario').value;
-    sAvatar = document.querySelector('#Avatar').value;
-    sEmail = document.querySelector('#Email').value;
-    sContraseña = document.querySelector('#Contraseña').value;
-    sGameRoom = document.querySelector('#GameRoom').value;
+  var sUsuario = "",
+    sAvatar = "",
+    sEmail = "",
+    sContraseña = "",
+    sGameRoom = "",
+    sUsuario = document.querySelector("#Usuario").value;
+  sAvatar = document.querySelector("#Avatar").value;
+  sEmail = document.querySelector("#Email").value;
+  sContraseña = document.querySelector("#Contraseña").value;
+  sGameRoom = document.querySelector("#GameRoom").value;
 
+  console.log("Email " + sEmail);
+  console.log("Contraseña " + sContraseña);
+  ç;
+  console.log("Usuario " + sUsuario);
 
-    console.log('Email ' + sEmail);
-    console.log('Contraseña ' + sContraseña);ç
-    console.log('Usuario ' + sUsuario);
-
-    console.log('Avatar ' + sAvatar);
-    console.log('GameRoom ' + sGameRoom);
-
-
-
-
+  console.log("Avatar " + sAvatar);
+  console.log("GameRoom " + sGameRoom);
 }
- 
